99d3222241f44888746909a8e0f02b32a6e45d25
doanvantung36 <117722263+doanvantung36@users.noreply.github.com>
2024-03-19 21:39:32 +0700

    Update Data.sql

--- Data.sql
+++ Data.sql
@@ -1,4 +1,4 @@
-﻿Drop database AssKTNC
+Drop database AssKTNC
 Create database AssKTNC
 
 CREATE TABLE Employees (
@@ -26,6 +26,36 @@ CREATE TABLE Products (
     employee_id INT,
     FOREIGN KEY (employee_id) REFERENCES Employees(employee_id)
 );
+
+-- Tạo bảng giỏ hàng
+CREATE TABLE ShoppingCart (
+    cart_id INT IDENTITY(1,1) PRIMARY KEY,
+    product_id INT,
+    quantity INT,
+    price DECIMAL(10, 2),
+    total_price DECIMAL(10, 2),
+    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+);
+
+-- Chèn dữ liệu vào bảng giỏ hàng
+INSERT INTO ShoppingCart (product_id, quantity, price, total_price)
+VALUES 
+(1, 2, 25.99, 51.98),  -- Áo sơ mi trắng, 2 cái, giá 25.99/cái, tổng tiền 51.98
+(3, 1, 59.99, 59.99);  -- Áo khoác dù xanh, 1 cái, giá 59.99/cái, tổng tiền 59.99
+
+CREATE TABLE CartDetail (
+    detail_id INT IDENTITY(1,1) PRIMARY KEY,
+    cart_id INT,
+    product_name VARCHAR(100),
+    total_price DECIMAL(10, 2),
+    shipping_unit VARCHAR(50),
+    FOREIGN KEY (cart_id) REFERENCES ShoppingCart(cart_id)
+);
+
+INSERT INTO CartDetail (cart_id, product_name, total_price, shipping_unit)
+VALUES 
+(1, 'Áo sơ mi trắng', 51.98, 'EMS'),
+(2, 'Áo khoác dù xanh', 59.99, 'FedEx');
 
 -- Chèn dữ liệu mẫu vào bảng Users
 INSERT INTO Users (username, password, role) 
